8ec71d0264b83b41c07b9afa31163930ad5990b8
refiantoyusuf <refiantoyusuf97@gmail.com>
2019-07-05 14:01:32 +0700

    first commit

---
+++ Model.py
@@ -0,0 +1,52 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="",
+  database="pos_perpus"
+)
+q = mydb.cursor()
+
+def show(tabel, vals=[]):
+	
+	# select = ['judul','image']
+
+	val = str(vals)
+	replace = (("'","`"),("[",""),("]","")) 
+	for x in replace:
+		val = val.replace(*x)	
+
+	
+	q.execute("SELECT "+val+" FROM `"+tabel+"`")
+	data = q.fetchall()
+
+	return data
+
+def all(tabel):
+
+	q.execute("SELECT * from `"+tabel+"`")
+	data = q.fetchall()
+
+	return data
+
+def add(tabel, vals=[]):
+
+	val = str(vals)
+	replace = (("[",""),("]",""),("None","NULL"))
+	for x in replace:
+		val = val.replace(*x)
+
+
+	q.execute("INSERT INTO `"+tabel+"` VALUES ("+val+");")
+	mydb.commit()
+	return
+
+def delete(tabel,id):
+
+	q.execute("DELETE FROM `"+tabel+"` WHERE `"+tabel+"`.`id` = "+id+"")
+	mydb.commit()
+	return
+
+
+print(add('buku',[None, 'books', None]))
